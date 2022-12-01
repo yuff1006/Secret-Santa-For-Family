4fd5f51be8afa78444330aa5bb9f61fbb513f520
Yuffie Hu <yuffieevans@gmail.com>
2022-12-01 15:00:55 -0600

    set up firebase hosting

---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getFireStore } from 'firebase/firestore';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyBI9LL8-g69lQrsivdF2f5sPH0_VJXWOEM',
+  authDomain: 'secret-santa-vandellens.firebaseapp.com',
+  projectId: 'secret-santa-vandellens',
+  storageBucket: 'secret-santa-vandellens.appspot.com',
+  messagingSenderId: '312803481031',
+  appId: '1:312803481031:web:b220f9c462e10f033335e2',
+  measurementId: 'G-FPNLKP53VX',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFireStore(app);
