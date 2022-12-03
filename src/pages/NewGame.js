aed21a851e3821ad339d4ce538af42b8a877849e
Yuffie Hu <yuffieevans@gmail.com>
2022-12-03 14:58:45 -0600

    added prelim logic to game creation

--- src/pages/NewGame.js
+++ src/pages/NewGame.js
@@ -1,4 +1,87 @@
+import { useState } from 'react';
+import { db } from '../firebase';
+import {
+  collection,
+  getDocs,
+  setDoc,
+  addDoc,
+  doc,
+  getDoc,
+} from 'firebase/firestore';
+import randomWords from 'random-words';
+
 export default function NewGame() {
+  const usersCollectionRef = collection(db, 'vandellens2022');
+  const [nameInput, setNameInput] = useState('');
+  const [gameToken, setGameToken] = useState('');
+  const [nameDisplay, setNameDisplay] = useState([]);
+  const [error, setError] = useState(false);
+  const handleNameInputChange = (evt) => {
+    setNameInput(evt.target.value);
+  };
+  const handleGameTokenChange = (evt) => {
+    setGameToken(evt.target.value);
+  };
+  const handleNameSubmit = (evt) => {
+    evt.preventDefault();
+    setNameDisplay((prev) => [...prev, nameInput]);
+    setNameInput('');
+  };
+  const handleAllDone = (evt) => {
+    const secretKeyPairs = secretKeyForNames(nameDisplay);
+    const namePairs = secretSantaMatching(nameDisplay);
+    evt.preventDefault();
+    addItem(gameToken, namePairs, secretKeyPairs)
+      .then((data) => console.log('success'))
+      .catch((err) => {
+        console.log(error);
+        setError(true);
+      });
+  };
+
+  const addItem = async (gameToken, pairs, secretKeys) => {
+    // const listCollectionRef = collection(db, 'data');
+    // return await addDoc(listCollectionRef, {
+    //   gameToken: gameToken,
+    //   pairs: pairs,
+    //   secretKeys: secretKeys,
+    // });
+    const docSnap = await getDoc(doc(db, 'data', gameToken));
+    if (docSnap.exists()) {
+      throw error;
+    } else {
+      return await setDoc(doc(db, 'data', gameToken), {
+        pairs: pairs,
+        secretKeys: secretKeys,
+      });
+    }
+  };
+  const secretKeyForNames = (nameList) => {
+    const secretKeyNamePairs = {};
+    for (let name of nameList) {
+      secretKeyNamePairs[name] = randomWords();
+    }
+    return secretKeyNamePairs;
+  };
+
+  const secretSantaMatching = (nameList) => {
+    const pairs = {};
+    const nameList2 = [];
+    for (let name of nameList) {
+      nameList2.push(name);
+    }
+    for (let i = 0; i < nameList2.length; i++) {
+      let randomizedIndex;
+      do {
+        randomizedIndex = Math.floor(Math.random() * nameList.length);
+      } while (nameList2[i] === nameList[randomizedIndex]);
+
+      pairs[nameList2[i]] = nameList[randomizedIndex];
+      nameList.splice(randomizedIndex, 1);
+    }
+    return pairs;
+  };
+
   return (
     <div className='new'>
       <form className='new__nameform'>
@@ -6,20 +89,38 @@ export default function NewGame() {
           type='text'
           className='new__input input-field'
           placeholder='Type one name at a time'
+          value={nameInput}
+          onChange={handleNameInputChange}
         />
-        <button type='submit' className='new__namebutton button'>
+        <button
+          type='submit'
+          className='new__namebutton button'
+          onClick={handleNameSubmit}
+        >
           Submit
         </button>
       </form>
       <div className='new__list'>
-        <p className='new__name'>Shelly</p>
-        <p className='new__name'>Shelly</p>
-        <p className='new__name'>Shelly</p>
-        <p className='new__name'>Shelly</p>
+        {nameDisplay.map((name) => (
+          <p key={name} className='new__list-item'>
+            {name}
+          </p>
+        ))}
       </div>
 
-      <p className='new__error'>Something went wrong...</p>
-      <button className='new__alldone button'>All Done!</button>
+      <div className='new__alldone-container'>
+        <input
+          type='text'
+          className='new__input input-field'
+          placeholder='Type in a Game Token'
+          value={gameToken}
+          onChange={handleGameTokenChange}
+        />
+        <button className='new__alldone button' onClick={handleAllDone}>
+          All Done!
+        </button>
+        {error && <p className='new__error'>Something went wrong...</p>}
+      </div>
     </div>
   );
 }
--- src/pages/Success.js
+++ src/pages/Success.js
@@ -2,11 +2,18 @@ import { NavLink } from 'react-router-dom';
 
 export default function Success() {
   return (
-    <>
+    <div className='success'>
       <h2 className='success__message'>Success!</h2>
-      <NavLink className='success__button button' to='/login'>
+      <p className='success__prompt'>Your Game Token is</p>
+      <p>HelloWorld</p>
+      <p className='success__prompt'>
+        And your participants' sercret keys are below:
+      </p>
+      <p></p>
+      <p className='success__prompt'>Take a screenshot for your convenience!</p>
+      <NavLink className='success__button button' to='/'>
         Back to Starting Page
       </NavLink>
-    </>
+    </div>
   );
 }
